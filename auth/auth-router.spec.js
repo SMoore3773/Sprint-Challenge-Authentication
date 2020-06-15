31e1331470786aa453ae6daec40a0355426a1ca2
SMoore3773 <Smooresculpture@gmail.com>
2020-06-14 22:01:35 -0400

    mvp

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,25 +1,24 @@
-const authRouter = require('./auth-router');
+
 const request = require('supertest');
 const Users = require('./authModel');
-const db = require('../database/dbConfig')
+const db = require('../database/dbConfig');
+const server = require('../api/server');
 
-describe('true', ()=>{
-    it('is true', ()=>{
-        expect(true).toBe(true)
-    })
-})
 
 describe('authrouter',()=>{
     describe('register',()=>{
-        it('should return 201 on creation', async ()=>{
+        it('should add user to db on creation', async ()=>{
             await Users.add({username:'user1', password:'password1'});
             const users = await db('users');
             expect(users).toHaveLength(1)
-        
+
+        })
+        it('should return 201 on registration', async ()=>{
+            const res = await request(server).post('/api/auth/register').send({username:'user1',password:'password1'});
+            expect(res.status).toBe(201)
         })
         it('should return user data', async()=>{
             let user = await Users.add({username:'user1', password:'password1'});
-
             expect(user.username).toBe('user1')
         })
         beforeEach(async ()=>{
@@ -33,15 +32,20 @@ describe('authrouter',()=>{
                 let user = await Users.findBy({username:newUser.username}).first();
                 expect(user.username).toBe('user1')
             })
-            it('should return an error if invalid user', async ()=>{
-                
-              const newUser = {username:'user1', password:'password1'};
-              await Users.add(newUser)
-              const badUser = {username:'userBad', password:'badPassword'};
-            //   let user = await request(authRouter).findBy({username:badUser.username})
-            //   let res = await request(authRouter).post('/login').send(badUser)
-            //   expect(res.status).toBe(401)
-                           
+            it('should return 200 on success', async ()=>{
+                const user = {username:'user1',password:'password1'}
+                await request(server).post('/api/auth/register').send(user);
+                const res = await request(server).post('/api/auth/login').send(user);
+                expect(res.status).toBe(200);
+            })
+            it('should return token on login', async()=>{
+                const user = {username:'user1',password:'password1'}
+                await request(server).post('/api/auth/register').send(user);
+                const res = await request(server).post('/api/auth/login').send(user);
+                expect(res.body).toHaveProperty('token')
+            })
+            beforeEach(async ()=>{
+                await db('users').truncate()
             })
         })
     })
